ce1ced77557f1d81877633b7a2c0044d78b8e7b8
feliperroman <juanfelipe1702@gmail.com>
2022-05-12 17:16:59 -0500

    Errores tres

--- resources/views/crud/venta/domiciliario/index.blade.php
+++ resources/views/crud/venta/domiciliario/index.blade.php
@@ -3,9 +3,10 @@
 @section('title', 'Domiciliarios')
 
 @section('content')
+
 <div class="container py-5">
     <div class="row">
-        <div class="col-12 col-md-10">
+        <div class="col-12">
             <div class="table-responsive p-2">
                 <div class="d-flex justify-content-between">
                     <h2 class="text-center">Domiciliarios</h2>
@@ -28,7 +29,7 @@
                                     <form action="{{route('domiciliario.store')}}" method="post" enctype="multipart/form-data">
                                         @csrf
                                         <div class="row">
-                                            @include('crud.venta.domiciliario.form',['modo'=>'Crear'])
+                                            @include('crud.venta.domiciliario.form',['modo'=>'errorRegistrar','tipo'=>'1'])
                                         </div>
                                 </div>
                             </div>
@@ -113,7 +114,7 @@
                                                             @csrf
                                                             {{method_field('PATCH')}}
                                                             <div class="row">
-                                                                @include('crud.venta.domiciliario.form',['modo'=>'Editar'])
+                                                                @include('crud.venta.domiciliario.form',['modo'=>'errorModificar','tipo'=>'2'])
                                                             </div>
 
 
@@ -136,20 +137,6 @@
                     </tbody>
                 </table>
             </div>
-        </div>
-        <div class="col-2 d-flex flex-column justify-content-center align-items-center">
-            <div class="bg-dark text-white text-center p-2 rounded-3 mb-2">
-                <p class="text-center fs-5 fw-bolder">Domiciliarios registrados</p>
-                <div class="d-flex justify-content-center align-items-center fs-3 mb-2">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="40" heigh="40" fill="currentColor" class="bi bi-person-fill me-1" viewBox="0 0 16 16">
-                        <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
-                    </svg>
-                    <p class="fw-bold m-0">{{ $total }}</p>
-                </div>
-            </div>
-            <button class="btn btn-warning">
-                Generar reporte
-            </button>
         </div>
     </div>
 </div>
